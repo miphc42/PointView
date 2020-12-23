98aab3a0fe48004b52b40ba5a8c564e66e66589d
miphc42 <philhc02@gmail.com>
2020-12-23 03:51:02 -0500

    comments

--- popup.js
+++ popup.js
@@ -2,14 +2,11 @@
 // document.getElementById('activate').onclick = function (){
 
 // }
-// Unique ID for the className.
 var MOUSE_VISITED_CLASSNAME = 'crx_mouse_visited';
 
-// Previous dom, that we want to track, so we can remove the previous styling.
 var prevDOM = null;
 var iframe = document.createElement('iframe');
 var p = document.createElement('p');
-// Mouse listener for any move event on the current document.
 document.addEventListener('mousemove', function (e) {
     let srcElement = e.srcElement;
     console.log(srcElement.result)
@@ -19,20 +16,13 @@ document.addEventListener('mousemove', function (e) {
         p.remove();
         console.log("REMOVE");
     }
-    // Lets check if our underlying element is a IMG.
     else if (srcElement.nodeName == 'A') {
 
-        // For NPE checking, we check safely. We need to remove the class name
-        // Since we will be styling the new one after.
         if (prevDOM != null) {
             prevDOM.classList.remove(MOUSE_VISITED_CLASSNAME);
         }
 
-        // Add a visited class name to the element. So we can style it.
         srcElement.classList.add(MOUSE_VISITED_CLASSNAME);
-
-        // The current element is now the previous. So we can remove the class
-        // during the next ieration.
         prevDOM = srcElement;
         console.info(srcElement.currentSrc);
         console.log("AAAA");
